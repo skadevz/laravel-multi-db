97845bad202f9028ce825efc04f3aa30d937a363
Ichsan Firdaus <ichsanfirdaus99@gmail.com>
2018-06-03 12:20:35 +0700

    update models

--- app/Ekstrakurikuler.php
+++ app/Ekstrakurikuler.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ekstrakurikuler extends Model
 {
+    protected $connection = 'sas_db';
+
     protected $table = 'ekstrakurikuler';
 
     protected $fillable = [
--- app/Siswa.php
+++ app/Siswa.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Siswa extends Model
 {
+    protected $connection = 'sas_db';
+
     protected $table = 'siswa';
 
     protected $fillable = [
